db68a9f0966ae13f6587dc676335e86f1839d7c7
Aguenana YASSER <75533635+yasseragn@users.noreply.github.com>
2024-07-17 02:38:57 +0100

    Update background.js

--- yasseragn-extension/js/background.js
+++ yasseragn-extension/js/background.js
@@ -7,13 +7,13 @@ async function get(name) {
 }
 
 async function set(name, value) {
-  return new Promise(function (resolve, reject) {
+  new Promise(function (resolve, reject) {
     chrome.storage.local.set({ [name]: value }, function () {
-      console.log(`Setting ${name}: `, value); // إضافة طباعة للمخرجات
       resolve();
     });
   });
 }
+
 async function sendToTab(action, tabId, data) {
   let response = await new Promise(function (resolve, reject) {
     data.action = action;
@@ -37,7 +37,7 @@ async function createOffscreen() {
 }
 
 async function sendToActiveTab(action, data = {}) {
-  return new Promise(async function (resolve, reject) {
+  new Promise(async function (resolve, reject) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       var activeTab = tabs[0];
       data.action = action;
@@ -49,7 +49,7 @@ async function sendToActiveTab(action, data = {}) {
 }
 
 async function getActiveTab() {
-  return new Promise(async function (resolve, reject) {
+  return await new Promise(async function (resolve, reject) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       var activeTab = tabs[0];
       resolve(activeTab);
@@ -71,6 +71,7 @@ function refreshActiveTab() {
 
 function createTab() {
   params = { url: "https://aadl3inscription2024.dz/", active: true };
+  // params = { url: "http://aadl3inscription2024.dz/", active: true };
   chrome.tabs.create(params, async function (tab) {
     await set("workingTab", tab.id);
   });
@@ -88,9 +89,9 @@ function startCounter() {
   let self = this;
   counter = 1;
   countProc = setInterval(async() => {
-    let isActive = await get("active");
+    let isActive = await get("active")
     if(isActive === false){
-      self.stopRefresh();
+      self.stopRefresh()
     }
     chrome.action.setBadgeText({ text: "" + counter });
     counter++;
@@ -105,54 +106,136 @@ async function checkContent(tabId) {
   return response;
 }
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  var _action = request.action;
-  switch (_action) {
-    case "refresh":
-      createTab();
-      break;
+async function sendTelegramMessage(botToken, chatId, message, photoUrl = null) {
+    if (photoUrl) {
+        const photoUrlTelegram = `https://api.telegram.org/bot${botToken}/sendPhoto`;
+        const photoData = {
+            chat_id: chatId,
+            photo: photoUrl,
+            caption: message,
+            parse_mode: 'Markdown'
+        };
+        try {
+            const response = await fetch(photoUrlTelegram, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(photoData)
+            });
 
-    case "stopRefresh":
-      stopRefresh();
-      break;
-
-    case "getTabId":
-      sendResponse({ tab: sender.tab.id });
-      break;
-  }
-  return true;
-});
-
-// Function to send Telegram message
-async function sendTelegramMessage(botToken, chatId, message) {
-    const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
-    const data = {
-        chat_id: chatId,
-        text: message,
-        parse_mode: 'Markdown'
-    };
-
-    try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
-
-        const result = await response.json();
-        if (response.ok) {
-            console.log('Message sent successfully:', result);
-        } else {
-            console.error('Failed to send message:', result);
+            const result = await response.json();
+            if (response.ok) {
+                console.log('Photo sent successfully:', result);
+            } else {
+                console.error('Failed to send photo:', result);
+            }
+        } catch (error) {
+            console.error('Error sending photo:', error);
         }
-    } catch (error) {
-        console.error('Error sending message:', error);
+    } else {
+        const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
+        const data = {
+            chat_id: chatId,
+            text: message,
+            parse_mode: 'Markdown'
+        };
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+
+            const result = await response.json();
+            if (response.ok) {
+                console.log('Message sent successfully:', result);
+            } else {
+                console.error('Failed to send message:', result);
+            }
+        } catch (error) {
+            console.error('Error sending message:', error);
+        }
     }
 }
 
-// Function to check some condition and send a message
+chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
+    let workingTab = await get("workingTab");
+    if (tabId == workingTab && changeInfo.status == "complete") {
+        clearInterval(countProc);
+        let hasLoaded = await checkContent(tabId);
+        if (!hasLoaded) {
+            chrome.action.setBadgeText({ text: "إعادة" });
+            setTimeout(async function () {
+                refreshTab(tabId);
+                startCounter();
+            }, 1000);
+        } else {
+            set("workingTab", -1);
+            createOffscreen();
+            chrome.notifications.create({
+                type: "basic",
+                title: "AADL3 Auto Refresh",
+                message: "صفحة التسجيل جاهزة للإستعمال",
+                iconUrl: "../img/favicon.png",
+            });
+            await set("active", false);
+            chrome.action.setBadgeText({ text: "" });
+            chrome.tabs.update(workingTab, { active: true }, async function (tab) {
+                // إرسال الرسالة الأولى
+                await checkConditionAndNotify();
+
+                // إرسال رسالة لملف content.js للضغط على زر "موافق"
+                await sendToTab("clickAgreeButton", tab.id, {});
+
+                // تحقق من وجود كلمة "تهانينا لكم" وأخذ لقطة شاشة إذا وجدت
+                let result = await sendToTab("checkCongratulations", tab.id, {});
+                if (result.found) {
+                    let screenshotUrl = result.screenshotUrl;
+                    let message = "*🚨AADL3 ALERT🚨* \n🔔تم تسجيلكم بنجاح🔔\nتهانينا لكم\nBY @yasseragnoff \n[انظم الى موقعنا الرسمي](https://www.yasseragn.com)";
+                    sendTelegramMessage(data.botToken, data.chatId, message, screenshotUrl);
+                }
+            });
+        }
+    }
+});
+
+chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
+    if (request.action === "takeScreenshot") {
+        chrome.tabs.captureVisibleTab(null, { format: "png" }, function (dataUrl) {
+            sendResponse({ screenshotUrl: dataUrl });
+        });
+        return true; // Required to indicate async response
+    }
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    var _action = request.action;
+    switch (_action) {
+        case "refresh":
+            createTab();
+            break;
+
+        case "stopRefresh":
+            stopRefresh();
+            break;
+
+        case "clearCookies":
+            stopRefresh();
+            break;
+
+        case "getTabId":
+            sendResponse({ tab: sender.tab.id });
+            break;
+    }
+    // return true  must be specified when returning an async response
+    return true;
+});
+
+// Function to send the first message when the page is ready
 async function checkConditionAndNotify() {
     chrome.storage.sync.get(['botToken', 'chatId', 'enableTelegram'], async function(data) {
         if (data.enableTelegram && data.botToken && data.chatId) {
@@ -184,32 +267,3 @@ async function checkConditionAndNotify() {
         }
     });
 }
-
-chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
-    let workingTab = await get("workingTab");
-    if (tabId == workingTab && changeInfo.status == "complete") {
-        clearInterval(countProc);
-        let hasLoaded = await checkContent(tabId);
-        if (!hasLoaded) {
-            setTimeout(async function () {
-                refreshTab(tabId);
-                startCounter();
-            }, 1000);
-        } else {
-            set("workingTab", -1);
-            createOffscreen();
-            chrome.notifications.create({
-                type: "basic",
-                title: "YASSERAGN AADL3 Auto Refresh & Telegram ALERT",
-                message: "صفحة التسجيل جاهزة للإستعمال, وافق على الشروط ",
-                iconUrl: "../img/favicon.png",
-            });
-            await set("active", false);
-            chrome.action.setBadgeText({ text: "" });
-            chrome.tabs.update(workingTab, { active: true }, async function (tab) {
-                // Send the message only when the page is ready
-                await checkConditionAndNotify();
-            });
-        }
-    }
-});
